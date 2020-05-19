6857293df336dde2eeafa99b7c1fd4fa8d933227
aitorgallardo <gmaitor@outlook.com>
2020-05-19 23:12:08 +0200

    contact page responsive

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,6 +7,7 @@ import Header from "../components/header"
 import Footer from "../components/footer"
 import SEO from "../components/seo"
 import GoogleMap from "../components/map"
+import './contact.css'
 
 
 const Contact = () => {
@@ -20,8 +21,8 @@ const Contact = () => {
       <Header />
       <div>
         <GoogleMap address={address} googleMapsApiKey={process.env.MAPS_APIKEY} />
-        <div className="info-container" style={{ display: 'flex', justifyContent: 'center', padding: '40px', paddingBottom:'100px' }}>
-          <div className="info-grid" style={{ display: `grid`, gridTemplateColumns: `300px 300px` }}>
+        <div className="info-container">
+          <div className="info-grid">
             <div>
               <h2>Horari</h2>
               <p>Tots els dies de la setmana</p>
