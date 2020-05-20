becfbd76bf3f7136b46b2501b25da32bac1ce28b
aitorgallardo <gmaitor@outlook.com>
2020-05-21 00:54:03 +0200

    sidebar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,7 +1,20 @@
 import React from "react";
 import { slide as Menu } from "react-burger-menu";
 import "./sidebar.css"
+import { Link } from "gatsby"
+import home_icon from "../icons/navbar/home_icon.svg"
+import about_us_icon from "../icons/navbar/about_us_icon.svg"
+import menu_icon from "../icons/navbar/menu_icon.svg"
+import contact_icon from "../icons/navbar/contact_icon.svg"
 
+const ListLink = props => (
+    <li >
+        <Link to={props.to} style={{ display: `flex`, alignItems: 'center' }}>
+            <img style={{ marginRight: '25px' }} id="menu-icon" src={props.icon_src} alt={props.icon_alt} widt="18px" height="18px" />
+            {props.children}
+        </Link>
+    </li>
+)
 
 export default class SideBar extends React.Component {
     constructor(props) {
@@ -24,21 +37,11 @@ export default class SideBar extends React.Component {
     render() {
         return (
             <Menu right isOpen={this.state.open} onStateChange={this.isMenuOpen}>
-                <a className="menu-item" href="/">
-                    Home
-        </a>
 
-                <a className="menu-item" href="/burgers">
-                    Burgers
-        </a>
-
-                <a className="menu-item" href="/pizzas">
-                    Pizzas
-        </a>
-
-                <a className="menu-item" href="/desserts">
-                    Desserts
-        </a>
+                <ListLink to="/" icon_src={home_icon} icon_alt="Home Icon">Inici</ListLink>
+                <ListLink to="/about" icon_src={about_us_icon} icon_alt="About Icon">Nosaltes</ListLink>
+                <ListLink to="/menu/cat" icon_src={menu_icon} icon_alt="Menu Icon">Carta</ListLink>
+                <ListLink to="/contact" icon_src={contact_icon} icon_alt="Contact Icon">Contacte</ListLink>
             </Menu>
         );
     }
