cf7cfeca69f3991ba804070ad2a955a27d3e29c2
aitorgallardo <gmaitor@outlook.com>
2020-05-21 17:49:05 +0200

    home full image

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,34 +6,59 @@ import Img from "gatsby-image"
 import Header from "../components/header"
 import Footer from "../components/footer"
 import SEO from "../components/seo"
+import BackgroundImage from "gatsby-background-image"
+import "./home.css"
 
-const Image = () => {
+
+// const Image = () => {
+//   const data = useStaticQuery(graphql`
+//       query {
+//         placeholderImage: file(relativePath: { eq: "elspescadors4.jpg" }) {
+//           childImageSharp {
+//             fluid(maxWidth: 5000) {
+//               ...GatsbyImageSharpFluid
+//             }
+//           }
+//         }
+//       }
+//     `)
+
+//   return <Img fluid={data.placeholderImage.childImageSharp.fluid} style={{ height: '100vh' }} />
+// }
+
+const HomePage = (props) => {
+
   const data = useStaticQuery(graphql`
-      query {
-        placeholderImage: file(relativePath: { eq: "elspescadors4.jpg" }) {
-          childImageSharp {
-            fluid(maxWidth: 5000) {
-              ...GatsbyImageSharpFluid
-            }
+    query {
+      indexImage: file(relativePath: { eq: "elspescadors4.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1800) {
+            ...GatsbyImageSharpFluid 
           }
         }
       }
-    `)
+    }
+`)
 
-  return <Img fluid={data.placeholderImage.childImageSharp.fluid} style={{ height: '100vh' }}/>
-}
-
-const HomePage = () => {
   return (
     <div>
-       <SEO title="Inici" />
-      <Header underline = "home"/>
-      <div>
+      <SEO title="Inici" />
+      <Header underline="home" />
+      {/* <div>
         <Image />
-      </div>
+      </div> */}
+      <BackgroundImage
+        className="main-backgroundImage"
+        fluid={data.indexImage.childImageSharp.fluid}
+      >
+        <div className="overlay">
+
+        </div>
+      </BackgroundImage>
       <Footer />
     </div>
 
   );
 }
 export default HomePage
+
