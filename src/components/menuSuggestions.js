c3100acb6507929f9d65b8f754dacbaab58a2b7c
aitor <gmaitor@outlook.com>
2021-05-14 14:49:39 +0200

    suggerencies en catala

--- src/components/menuSuggestions.js
+++ src/components/menuSuggestions.js
@@ -5,21 +5,23 @@ import ModalImage from "react-modal-image";
 suggestions is changed to menu. It encapsultes his own state and just this component]
 is being rerendered instead of the whole parent*/ 
 
-const MenuSuggestions = ({menuImage,suggestionsImage, options}) => {
+const MenuSuggestions = ({menuImage1,menuImage2,suggestionsImage, options}) => {
   
-  const { menuName,suggestionsName,imgAlt } = options;  
+  const { menuName,suggestionsName,imgAltMenu,imgAltWine,isWine } = options;  
   const [suggestions, setSuggestions] = useState(false)
-  const [image, setImage] = useState(menuImage)
+  const [image1, setImage1] = useState(menuImage1)
+  const [image2, setImage2] = useState(menuImage2)
 
 
 
   const showSuggestions = (isShown) => {
     if(isShown){
         setSuggestions(true)
-        setImage(suggestionsImage)
+        setImage1(suggestionsImage)
     }else{
         setSuggestions(false)
-        setImage(menuImage)
+        setImage1(menuImage1)
+        //setImage2(image2)
     }
   }
 
@@ -66,13 +68,23 @@ const MenuSuggestions = ({menuImage,suggestionsImage, options}) => {
           </div>
         </div>
       )}
-      {image && (
+      {image1 && (
         <ModalImage
           className="menu-image"
-          small={image}
-          large={image}
+          small={image1}
+          large={image1}
           hideZoom={true}
-          alt={imgAlt}
+          alt={imgAltMenu}
+        />
+      )}
+      {isWine && (<h2 style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '100px'}}>{isWine}</h2>)}
+      {image2 && (
+        <ModalImage
+          className="menu-image"
+          small={image2}
+          large={image2}
+          hideZoom={true}
+          alt={imgAltWine}
         />
       )}
     </>
--- src/pages/menu/cat.js
+++ src/pages/menu/cat.js
@@ -3,6 +3,7 @@ import { useStaticQuery, graphql } from "gatsby"
 import ModalImage from "react-modal-image";
 import LanguageNav from "../../components/language_nav"
 import MenuLayout from "../../components/menuLayout"
+import MenuSuggestions from "../../components/menuSuggestions"
 
 
 
@@ -30,12 +31,23 @@ const MenuCat = () => {
 
   let image1 = null
   let image2 = null
+  let suggestionsImage = null;
+
+  const options = {
+    menuName:'Menu',
+    suggestionsName:'Suggestions',
+    imgAltMenu:'Carta',
+    imgAltWine:'Carta Vins',
+    isWine:'Vins'
+  }
 
   data.allFile.edges.map(({node})=>{
     if(node.base.includes('menu')){
       image1 = node.childImageSharp.fluid.src;
-    }else{
+    }else if(node.base.includes('vins')){
       image2 = node.childImageSharp.fluid.src;
+    }else{
+      suggestionsImage = node.childImageSharp.fluid.src;
     }
   })
 
@@ -44,21 +56,7 @@ const MenuCat = () => {
     <MenuLayout>
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
       <LanguageNav selected="cat"/>
-      <div>
-        <ModalImage
-          small={image1}
-          large={image1}
-          hideZoom={true}
-          alt="Carta"
-        />
-        <h2 style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '100px'}}>Vins</h2>
-        <ModalImage
-          small={image2}
-          large={image2}
-          hideZoom={true}
-          alt="Carta Vins"
-        />
-      </div>
+      <MenuSuggestions menuImage1={image1} menuImage2={image2} suggestionsImage={suggestionsImage} options={options}/>
     </div>
       </MenuLayout>
   </>
